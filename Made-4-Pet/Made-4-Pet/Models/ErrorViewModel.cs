02e6a1822a83b4bb848da5e472e7ba4142d1c9ca
Eric-Pessoa <pessoa.eric@gmail.com>
2021-11-20 18:35:30 -0300

    Scaffold

---
+++ Made-4-Pet/Made-4-Pet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Made_4_Pet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
