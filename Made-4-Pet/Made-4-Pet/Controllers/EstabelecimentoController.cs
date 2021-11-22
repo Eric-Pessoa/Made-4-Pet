7e3f9814c3819e8551c8049f081b9591a3643057
giovannadml <gih_mello@outlook.com>
2021-11-22 11:01:57 -0300

    layout logado e ajustes no scss

---
+++ Made-4-Pet/Made-4-Pet/Controllers/EstabelecimentoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Made_4_Pet.Controllers
+{
+    public class EstabelecimentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
