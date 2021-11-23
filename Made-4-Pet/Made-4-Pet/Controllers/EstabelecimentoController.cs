6c2cd6fd3a60a1712d0206d6b8ed29678315ee49
Eric-Pessoa <pessoa.eric@gmail.com>
2021-11-23 15:15:22 -0300

    chore: mudanças para realizar a busca

--- Made-4-Pet/Made-4-Pet/Controllers/EstabelecimentoController.cs
+++ Made-4-Pet/Made-4-Pet/Controllers/EstabelecimentoController.cs
@@ -6,11 +6,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FireSharp.Interfaces;
+using FireSharp.Config;
+using FireSharp.Response;
+using Newtonsoft.Json.Linq;
 
 namespace Made_4_Pet.Controllers
 {
     public class EstabelecimentoController : Controller
     {
+        IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = "IhjTIVux94nQcsVA6MOiP5ndaQEc2vSdJ83bfbce",
+            BasePath = "https://made4pet-cb9c3-default-rtdb.firebaseio.com/"
+        };
+        IFirebaseClient client;
+        JObject json;
+        List<Estabelecimento> estabelecimentos;
+
+
         public IActionResult Index()
         {
             Cliente cliente = new Cliente();
@@ -20,9 +34,49 @@ namespace Made_4_Pet.Controllers
             ViewBag.horarios = horarios;
             return View();
         }
+        [HttpGet]
         public IActionResult ProcuraPorServico()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult ProcuraPorServico(string nomeBusca = null, string categoria = null)
+        {
+            client = new FireSharp.FirebaseClient(config);
+            estabelecimentos = new List<Estabelecimento>();
+            FirebaseResponse response = client.Get("/estabelecimento/");
+
+            if (response.Body != "null")
+            {
+                json = JObject.Parse(response.Body);
+                foreach (var i in json)
+                {
+                    var estabelecimento = i.Value.ToObject<Estabelecimento>();
+                    estabelecimentos.Add(estabelecimento);
+                }
+
+                if (nomeBusca != null)
+                {
+                    var filter = estabelecimentos.FindAll(i => i.Nome.ToLower().Contains(nomeBusca.ToLower()));
+                    return View(filter);
+                }
+                else if(categoria != null)
+                {
+                    switch (categoria)
+                    {
+                        case "banhoETosa":
+                            var filter = estabelecimentos.FindAll(i => i.Nome.ToLower().Contains(categoria.ToLower()));
+                            return;
+                        case "saude":
+                            return;
+                        case "crecheEHotel":
+                            return;
+                    }
+                }
+            }
+            return View();
+        }
+
     }
 }
